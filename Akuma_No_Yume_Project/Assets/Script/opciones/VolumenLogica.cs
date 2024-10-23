ba37554b1118542d0e0dadac00610e4db1944884
04martad <marta.de.marcos@estudiante.epsum.school>
2024-10-23 19:03:35 +0200

    brillo y volumen

---
+++ Akuma_No_Yume_Project/Assets/Script/opciones/VolumenLogica.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class volume_logic : MonoBehaviour
+{
+
+    public Slider slider;
+    public float sliderValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("audioVol", 0.5f);
+        AudioListener.volume = slider.value;
+    }
+
+    public void ChangeSlider(float value)
+    {
+        sliderValue = value;
+        PlayerPrefs.SetFloat("audioVol", sliderValue);
+        AudioListener.volume = sliderValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
