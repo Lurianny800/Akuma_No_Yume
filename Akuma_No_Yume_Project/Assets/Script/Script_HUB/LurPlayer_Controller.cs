c57f7aa62f21cf4e12e3285e9e309e789cd9d4dd
Lurianny800 <lurianny.nicolle.malave@estudiante.epsum.school>
2024-10-24 18:37:00 +0200

    Revert "Script de movimiento escrito"
    This reverts commit 8f92deccdd2c0a3fb745de8abaa734720e99e06f.

--- Akuma_No_Yume_Project/Assets/Script/Script_HUB/LurPlayer_Controller.cs
+++ Akuma_No_Yume_Project/Assets/Script/Script_HUB/LurPlayer_Controller.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class LurPlayer_Controller : MonoBehaviour
 {
-    public float fps = 5f;
-    public float moveSpeed = 5f;
-    public Rigidbody rb;
-    public float moveForce = 10f;
-    public CharacterController controller;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +14,5 @@ public class LurPlayer_Controller : MonoBehaviour
     void Update()
     {
         
-    }
-    void Movimiento3() //Movimiento físico controlado sin pérdida de precisión.
-    {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical"); ;
-        Vector3 move = new Vector3(moveX, moveY, 0) * moveSpeed * Time.deltaTime; rb.MovePosition(rb.position + move);
     }
 }
