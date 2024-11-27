ecb7b5e763428cbdb09a512b9554e94da55b2511
Lurianny800 <lurianny.nicolle.malave@estudiante.epsum.school>
2024-11-27 14:26:14 +0100

    Añadir coleccionables
    Falta hacer el documento, añadir una corrutina y atributos. Organización de carpetas.

---
+++ Akuma_No_Yume_Project/Assets/Script/Script_HUB/GameManager_Lur.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_Lur : MonoBehaviour
+{
+    public static GameManager_Lur instance;
+    public int score = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Asegurarte de que persista entre escenas
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        Debug.Log("Coleccionable: " + score);
+    }
+}
